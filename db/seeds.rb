9c2a8097be1172840db04d7a30b91075dfc7bc6d
grantbradshaw <grant.bradshaw@outlook.com>
2016-02-02 21:12:10 +0000

    Added db files

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.create!(
+  title: 'Away We Go',
+  director: 'Sam Mendes',
+  runtime_in_minutes: 98,
+  description: 'A couple who is expecting their first child travel around the U.S. in order to find a perfect place to start their family. Along the way, they have misadventures and find fresh connections with an assortment of relatives and old friends who just might help them discover "home" on their own terms for the first time.',
+  poster_image_url: 'http://ecx.images-amazon.com/images/I/51rrQ47zVIL._SY344_BO1,204,203,200_.jpg',
+  release_date: Date.new(2009, 6, 26)
+  )
