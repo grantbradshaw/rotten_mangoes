94c175dd27756aff44b878544488bb6ce5088017
grantbradshaw <grant.bradshaw@outlook.com>
2016-02-02 21:11:32 +0000

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = 'af46e23ddf2cec97055193dffffb586dd4f1f798473cf98de17d33375f5369ddc953ed77ad2d49cab145ba358c5bceb0122455cbcaf53629939690cae8048f25'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+RottenMangoes::Application.routes.draw do
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
+end
